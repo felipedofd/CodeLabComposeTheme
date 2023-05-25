72f0e43715bead64d26a7ca8f9d602462dfb1e2f
Felipe Anzolin <felipe.anzolin@cognizant.com>
2023-05-25 19:35:58 -0300

    Ao transmitir uma função e não um estado para OnboardingScreen, estamos tornando essa composição mais reutilizável e protegendo o estado contra mutação por outras composições. Em geral, é simples. Um bom exemplo é como a visualização de integração precisa ser modificada para chamar OnboardingScreen Atribuir onContinueClicked a uma expressão lambda vazia significa "não fazer nada", o que é perfeito para uma visualização.

--- app/src/main/java/com/example/codelabcompose/MainActivity.kt
+++ app/src/main/java/com/example/codelabcompose/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.codelabcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,13 +36,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(
-    modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")
+private fun MyApp(modifier: Modifier = Modifier) {
+
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+
+        if (shouldShowOnBoarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun MyAppPreview() {
+    CodeLabComposeTheme {
+        MyApp(Modifier.fillMaxSize())
+
+    }
+
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
 ) {
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun GreetingsPreview() {
+    CodeLabComposeTheme {
+        Greetings()
     }
 }
 
@@ -51,9 +111,11 @@ fun Greeting(name: String) {
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text("Hello,")
                 Text(text = name)
             }
@@ -71,6 +133,15 @@ fun Greeting(name: String) {
 fun GreetingPreview() {
     CodeLabComposeTheme {
         MyApp()
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    CodeLabComposeTheme() {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
