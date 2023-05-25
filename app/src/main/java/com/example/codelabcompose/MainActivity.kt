0f8e569ae92f6367297ab108b1031a68621fbfa5
Felipe Anzolin <felipe.anzolin@cognizant.com>
2023-05-25 11:32:21 -0300

    o modificador padding aplicará um pouco de espaço ao redor do elemento que ele decora. Você pode criar um modificador de padding com Modifier.padding().

--- app/src/main/java/com/example/codelabcompose/MainActivity.kt
+++ app/src/main/java/com/example/codelabcompose/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.codelabcompose.ui.theme.CodeLabComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +36,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.primary) {
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
     }
 }
