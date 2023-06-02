2d5e029c1f63e989e28da05a41cdccb6113e119d
Felipe Anzolin <felipedofd@hotmail.com>
2023-06-02 17:51:41 -0300

    style any of the composables so far and yet you got a decent default, including dark mode support! Let's look into what BasicsCodelabTheme and MaterialTheme are.
    If you open the ui/theme/Theme.kt file
    
    MaterialTheme is a composable function that reflects the styling principles from the Material design specification. That styling information cascades down to the components that are inside its content, which may read the information to style themselves. In your UI, you are already using BasicsCodelabTheme
    The Text composable in the example above sets a new TextStyle. You can create your own TextStyle, or you can retrieve a theme-defined style by using MaterialTheme.typography, which is preferred. This construct gives you access to the Material-defined text styles, such as displayLarge, headlineMedium, titleSmall, bodyLarge, labelMedium etc. In your example, you use the headlineMedium style defined in the theme.
    
    You can find everything related to the current theme in the files inside the ui/theme folder. For example, the default colors that we have been using so far are defined in Color.kt.

--- app/src/main/java/com/example/codelabcompose/MainActivity.kt
+++ app/src/main/java/com/example/codelabcompose/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.codelabcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -24,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.codelabcompose.ui.theme.CodeLabComposeTheme
@@ -112,19 +116,28 @@ private fun Greeting(name: String) {
     var expanded by remember { mutableStateOf(false) }
 
     val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp, label = ""
+        if (expanded) 48.dp else 0.dp, label = "",
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
     )
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -150,6 +163,19 @@ fun GreetingPreview() {
 fun OnboardingPreview() {
     CodeLabComposeTheme() {
         OnboardingScreen(onContinueClicked = {})
+    }
+}
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    CodeLabComposeTheme {
+        Greetings()
     }
 }
 
